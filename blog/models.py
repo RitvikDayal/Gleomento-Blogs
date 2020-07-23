2a3f8f16681603947e30661359d63797837a8f96
RitvikDayal-github <ritvikr1605@gmail.com>
2020-07-24 03:42:49 +0530

    Updates password reset functionality

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    summary = models.CharField(max_length=100)
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def save(self, *args, **kwargs):
+        if self.content:
+            self.summary = self.content[:60]
+        super(Post, self).save(*args, **kwargs)
+
     def __str__(self):
         return 'Title: {}\nAuthor: {}'.format(self.title, self.author)
+    
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
     
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from .views import PostListView, UserPostListView, PostDetailView, PostCreateView,PostDeleteView, PostUpdateView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='gleo-home'),
-    path('blog/', views.blog, name='gleo-blog'),
+    path('blog/user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('blog/', PostListView.as_view(), name='gleo-blog'),
+    path('blog/post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('blog/post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('blog/post/new/', PostCreateView.as_view(), name='post-create'),
+    path('blog/post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='gleo-about'),
 ]
